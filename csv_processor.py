9e3604293bb2b0996bd5f7103167e3cf7f98ca1e
yungalyx <52767711+yungalyx@users.noreply.github.com>
2020-12-14 23:36:12 -0800

    knn

--- csv_processor.py
+++ csv_processor.py
@@ -8,55 +8,89 @@ from neural_net import NeuralNet
 
 class CSV_Processor():
 
-  def process_csv(self, Xpath, yPath):
-    # pre-processing X train data
-    # ROOT_DIR = os.path.abspath(os.curdir)
-    # csvtrainingDir = ROOT_DIR
-    all_X_files = glob.glob(Xpath)
-    # print(all_X_files)
+    def __init__(self, knnMode=False):
+        self.knnMode = knnMode
 
-    main_X_file = []
-    for file in all_X_files:
-        df = pd.read_csv(file, index_col=None, header=0)
-        # print(file)
-        for col in df.columns:
-            if 'time' in col or 'id' in col or 'type' in col:
-                df.drop(col, axis=1, inplace=True)
+    def process_csv(self, Xpath, yPath):
+        # pre-processing X train data
+        # ROOT_DIR = os.path.abspath(os.curdir)
+        # csvtrainingDir = ROOT_DIR
+        all_X_files = glob.glob(Xpath) 
+        # print(all_X_files)
 
-            if 'role' in col:
-                # convert 'role' values to integers
-                for idx in range(11):
-                    if df.loc[idx, col] == 'agent':
-                        df.loc[idx, col] = 1
+        main_X_file = []
+        for file in all_X_files:
+            df = pd.read_csv(file, index_col=None, header=0)
+            # print(file)
+            flag = 0
+            for col in df.columns:  
+                if 'role' in col:
+                    if isinstance(df.loc[0, col], float):
+                        df.drop(col, axis=1, inplace=True)
+                    elif 'agent' in df.loc[0, col]:
+                        # print(df.loc[0, col])
+                        flag = 1
+                        df.drop(col, axis=1, inplace=True)
+                    else: 
+                        df.drop(col, axis=1, inplace=True)
+                elif flag == 1:
+                    if 'y' in col:
+                        print(col)
+                        flag = 0
+                    if 'x' in col: 
+                        print(col)
+                        continue
                     else:
-                        df.loc[idx, col] = 0
-        array = df.to_numpy()
-        flattened_array = array.flatten()
-        main_X_file.append(flattened_array)
+                        df.drop(col, axis=1, inplace=True)
+                else:
+                    df.drop(col, axis=1, inplace=True)
 
-    # convert main_X_file to np.array
-    main_X_array = np.array(main_X_file)
-    # print(main_X_array)
+            print(df)        
+
+                # flag--
+                # if flag < 0: 
+                #     df.drop(col, axis=1, inplace=True)
+
+                # if 'agent' in col:
+                #     flag = 2
+                # if 'time' in col or 'id' in col or 'type' in col:
+                #     df.drop(col, axis=1, inplace=True)
+
+                # if 'role' in col:
+                #     # convert 'role' values to integers
+                #     for idx in range(11):
+                #         if df.loc[idx, col] == 'agent':
+                #             df.loc[idx, col] = 1
+                #         else:
+                #             df.loc[idx, col] = 0
+                
+            array = df.to_numpy()
+            flattened_array = array.flatten()
+            main_X_file.append(flattened_array)
+
+        # convert main_X_file to np.array
+        main_X_array = np.array(main_X_file)
+        # print(main_X_array)
 
 
-    # pre-processing Y train data
-    all_y_files = glob.glob(yPath)
-    # print(all_y_files)
+        # pre-processing Y train data
+        all_y_files = glob.glob(yPath)
+        # print(all_y_files)
 
-    main_y_file = []
-    for file in all_y_files:
-        df = pd.read_csv(file, index_col=None, header=0)
-        # print(file)
-        for col in df.columns:
-            if 'time' in col:
-                df.drop(col, axis=1, inplace=True)
+        main_y_file = []
+        for file in all_y_files:
+            df = pd.read_csv(file, index_col=None, header=0)
+            # print(file)
+            for col in df.columns:
+                if 'time' in col:
+                    df.drop(col, axis=1, inplace=True)
 
-        array = df.to_numpy()
-        flattened_array = array.flatten()
-        main_y_file.append(flattened_array)
+            array = df.to_numpy()
+            flattened_array = array.flatten()
+            main_y_file.append(flattened_array)
 
-    # convert main_y_file to np.array
-    main_y_array = np.array(main_y_file)
-    # print(main_y_array)
+        # convert main_y_file to np.array
+        main_y_array = np.array(main_y_file)
+        # print(main_y_array)
 
-    return main_X_array, main_y_array
+        return main_X_array, main_y_array
--- main.py
+++ main.py
@@ -1,34 +1,59 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pylab as plt
+import argparse
 import time
 import os
 import glob
 from neural_net import NeuralNet
 from csv_processor import CSV_Processor
 
-# pre-processing X train data
-ROOT_DIR = os.path.abspath(os.curdir)
-all_X_train_files = ROOT_DIR + "/data/train/X/*.csv"
-all_Y_train_files = ROOT_DIR + "/data/train/y/*.csv"
 
-all_X_val_files = ROOT_DIR + "/data/val/X/*.csv"
-all_Y_val_files = ROOT_DIR + "/data/val/y/*.csv"
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-q', '--question', required=True)
 
-csv_processor = CSV_Processor()
+    io_args = parser.parse_args()
+    question = io_args.question
 
-Xtrain, ytrain = csv_processor.process_csv(all_X_train_files, all_Y_train_files)
-Xval, yval = csv_processor.process_csv(all_X_val_files, all_Y_val_files)
+    if question == "1":
 
-print(Xtrain)
-print(ytrain)
-print(Xval)
-print(yval)
+        ROOT_DIR = os.path.abspath(os.curdir)
+        all_X_train_files = ROOT_DIR + "/data/train/X/*.csv"
+        all_Y_train_files = ROOT_DIR + "/data/train/y/*.csv"
+
+        all_X_val_files = ROOT_DIR + "/data/val/X/*.csv"
+        all_Y_val_files = ROOT_DIR + "/data/val/y/*.csv"
+
+        print("starting...")
+
+        csv_processor = CSV_Processor(knnMode=True)
+
+        Xtrain, ytrain = csv_processor.process_csv(all_X_train_files, all_Y_train_files)
+        Xval, yval = csv_processor.process_csv(all_X_val_files, all_Y_val_files)
+
+        print(Xtrain)
+        print(ytrain)
+        print(Xval)
+        print(yval)
+
+        pd.DataFrame(Xtrain).to_csv('Xtrainprocessed.csv')
+        pd.DataFrame(ytrain).to_csv('ytrainprocessed.csv')
+        pd.DataFrame(Xval).to_csv('Xvalprocessed.csv')
+        pd.DataFrame(yval).to_csv('yvalprocessed.csv')
+
+    if question == '2':
+        df=pd.read_csv('Xtrainprocessed.csv', sep=',', header=0, index_col=0)
+        print(df.values)
+        
+
+
+
 
 # hidden_layer_sizes = [50]
 # model = NeuralNet(hidden_layer_sizes)
 
-print("made it")
+
 
 # t = time.time()
 # model.fit(Xtrain,ytrain)

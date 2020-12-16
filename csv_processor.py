e14c7a6881ad324fee652f72ddd72bd94c0d4fa2
yungalyx <52767711+yungalyx@users.noreply.github.com>
2020-12-16 15:37:56 -0800

    final

--- csv_processor.py
+++ csv_processor.py
@@ -4,20 +4,75 @@ import numpy as np
 import time
 import os
 import glob
+import re
 from neural_net import NeuralNet
 
+ROOT_DIR = os.path.abspath(os.curdir)
+
+sample_submission = ROOT_DIR + "\sample_submission.csv"
 
 class CSV_Processor():
 
     def __init__(self, knnMode=False):
         self.knnMode = knnMode
 
-    def process_csv(self, Xpath, yPath):
-        # pre-processing X train data
-        # ROOT_DIR = os.path.abspath(os.curdir)
-        # csvtrainingDir = ROOT_DIR
+    def process_output_csv(self, path):
+        df = pd.read_csv(path, index_col=None, header=0)
+        return df
+
+    def to_kaggle_csv(self, y):
+        retcsv = y.flatten()
+        retcsv2 = pd.DataFrame(retcsv, columns=["location"])
+        retcsv2.to_csv("y_pred.csv", index=False)
+
+        df = self.process_output_csv(sample_submission)
+        df.drop(df.columns[1], axis=1, inplace=True)
+        df2 = df.join(retcsv2)
+        df2.to_csv('y_output.csv', index=False)
+
+    def process_test_csv(self, Xpath):
         all_X_files = glob.glob(Xpath)
         # print(all_X_files)
+
+        main_X_file = []
+        for file in all_X_files:
+            df = pd.read_csv(file, index_col=None, header=0)
+            flag = 0
+            for col in df.columns:
+                if 'role' in col:
+                    if isinstance(df.loc[0, col], float):
+                        df.drop(col, axis=1, inplace=True)
+                    elif 'agent' in df.loc[0, col]:
+                        # print(df.loc[0, col])
+                        flag = 1
+                        df.drop(col, axis=1, inplace=True)
+                    else:
+                        df.drop(col, axis=1, inplace=True)
+                elif flag == 1:
+                    if 'type' in col:
+                        df.drop(col, axis=1, inplace=True)
+                    elif 'y' in col:
+                        # print(col)
+                        flag = 0
+                        continue
+                    elif 'x' in col:
+                        # print(col)
+                        continue
+                    else:
+                        df.drop(col, axis=1, inplace=True)
+                else:
+                    df.drop(col, axis=1, inplace=True)
+
+            array = df.to_numpy()
+            flattened_array = array.flatten()
+            main_X_file.append(flattened_array)
+
+        # convert main_X_file to np.array
+        main_X_array = np.array(main_X_file)
+        return main_X_array
+
+    def process_csv(self, Xpath, yPath):
+        all_X_files = glob.glob(Xpath)
 
         main_X_file = []
         for file in all_X_files:
@@ -48,25 +103,6 @@ class CSV_Processor():
                         df.drop(col, axis=1, inplace=True)
                 else:
                     df.drop(col, axis=1, inplace=True)
-
-            # print(df)
-
-            # flag--
-            # if flag < 0:
-            #     df.drop(col, axis=1, inplace=True)
-
-            # if 'agent' in col:
-            #     flag = 2
-            # if 'time' in col or 'id' in col or 'type' in col:
-            #     df.drop(col, axis=1, inplace=True)
-
-            # if 'role' in col:
-            #     # convert 'role' values to integers
-            #     for idx in range(11):
-            #         if df.loc[idx, col] == 'agent':
-            #             df.loc[idx, col] = 1
-            #         else:
-            #             df.loc[idx, col] = 0
 
             array = df.to_numpy()
             flattened_array = array.flatten()
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import argparse
 import time
 import os
 import glob
+import re
 from ast import literal_eval
 from neural_net import NeuralNet
 from csv_processor import CSV_Processor
@@ -18,6 +19,8 @@ if __name__ == '__main__':
     io_args = parser.parse_args()
     question = io_args.question
 
+    csv_processor = CSV_Processor(knnMode=True)
+
     if question == "1":
 
         ROOT_DIR = os.path.abspath(os.curdir)
@@ -27,94 +30,112 @@ if __name__ == '__main__':
         all_X_val_files = ROOT_DIR + "/data/val/X/*.csv"
         all_Y_val_files = ROOT_DIR + "/data/val/y/*.csv"
 
+        all_X_test_files = ROOT_DIR + "/data/test/X/*.csv"
+
         print("starting...")
 
-        csv_processor = CSV_Processor(knnMode=True)
 
-        Xtrain, ytrain = csv_processor.process_csv(all_X_train_files, all_Y_train_files)
-        Xval, yval = csv_processor.process_csv(all_X_val_files, all_Y_val_files)
+        #Xtrain, ytrain = csv_processor.process_csv(all_X_train_files, all_Y_train_files)
+        #Xval, yval = csv_processor.process_csv(all_X_val_files, all_Y_val_files)
+        Xtest = csv_processor.process_test_csv(all_X_test_files)
 
-        print(Xtrain)
-        print(ytrain)
-        print(Xval)
-        print(yval)
-
-        pd.DataFrame(Xtrain).to_csv('Xtrainprocessed.csv')
-        pd.DataFrame(ytrain).to_csv('ytrainprocessed.csv')
-        pd.DataFrame(Xval).to_csv('Xvalprocessed.csv')
-        pd.DataFrame(yval).to_csv('yvalprocessed.csv')
+        pd.DataFrame(Xtest).to_csv('Xtestprocessed.csv')
+        #pd.DataFrame(Xtrain).to_csv('Xtrainprocessed.csv')
+        #pd.DataFrame(ytrain).to_csv('ytrainprocessed.csv')
+        #pd.DataFrame(Xval).to_csv('Xvalprocessed.csv')
+        #pd.DataFrame(yval).to_csv('yvalprocessed.csv')
 
     if question == '2':
         dfx=pd.read_csv('Xtrainprocessed.csv', sep=',', header=0, index_col=0)
         dfy = pd.read_csv('ytrainprocessed.csv', sep=',', header=0, index_col=0)
 
+        dfxtest = pd.read_csv('Xtestprocessed.csv', sep=',', header=0, index_col=0)
         dfyval = pd.read_csv('yvalprocessed.csv', sep=',', header=0, index_col=0)
         
         # yval cleaning:
         yval = dfyval.to_numpy()
-        
+        Xtest = dfxtest.to_numpy()
   
         X = dfx.to_numpy() 
         print(X.shape)    
         y = dfy.to_numpy()
         print(y.shape)
 
-        model = KNN(k=3)
 
-        model.fit(X, y)
+        for n in range(5):
+            bestErr = 100000
+            bestn = 0
 
-        print('predicting...')
-      
-        y_pred = model.predict(X)
+            model = KNN(k=n)
 
-        # print(y_pred)
+            model.fit(X, y)
 
-        test_error_abs = 0
-        test_error_mse = 0
-        for i in range(len(y_pred)):
-            label = y[i]
-            label = label[0]
-            if label[1] == ' ':
-                # print('has a space', label)
-                label = label.replace(' ', '', 1) 
-            label = label.replace('    ', ' ').replace('   ', ' ').replace('  ', ' ').replace(' ', ', ')
-            y_temp = literal_eval(label)
-            arr = np.array(y_temp)
-            if (len(arr)) < 60:
-                arr = np.pad(arr, (0, 60-len(arr)), 'constant', constant_values=(0,0))
-            
-            test_error_mse += np.sum((y_pred[i] - arr)**2)
-            test_error_abs += np.sum(np.abs(y_pred[i] - arr))
+            print('predicting...')
         
-        print("KNN Absolute Value Error: %.3f" % test_error_abs)
-        print("KNN Mean Squared Error: %.3f" % test_error_mse)
+            y_pred = model.predict(Xtest)
 
-        # saving results to a csv submission file
-        print(y_pred[0])
+            # print(y_pred)
 
+            test_error_abs = 0
+            test_error_mse = 0
+            for i in range(len(y_pred)):
+                label = y[i]
+                label = label[0]
+                if label[1] == ' ':
+                    # print('has a space', label)
+                    label = label.replace(' ', '', 1) 
+                label = label.replace('    ', ' ').replace('   ', ' ').replace('  ', ' ').replace(' ', ', ')
+                y_temp = literal_eval(label)
+                arr = np.array(y_temp)
+                if (len(arr)) < 60:
+                    arr = np.pad(arr, (0, 60-len(arr)), 'constant', constant_values=(0,0))
+                
+                test_error_mse += np.sum((y_pred[i] - arr)**2)
+                test_error_abs += np.sum(np.abs(y_pred[i] - arr))
+            
+            print("=====", n, "=====")
+            print("KNN Absolute Value Error: %.3f" % test_error_abs)
+            print("KNN Mean Squared Error: %.3f" % test_error_mse)
+
+            if test_error_mse < bestErr:
+                bestErr = test_error_mse
+                bestn = n 
+        print(test_error_mse)
+        print(bestn)
+
+            # saving results to a csv submission file
+            # print(y_pred)
+        please = np.array(y_pred)
+        csv_processor.to_kaggle_csv(please)
+        
+        
+        '''
         idcolumn = []
         locolumn = []
         # for idn in range(X.shape[0]):  # number of samples 
-        for idn in range(19):
+        
+        for idn in range(20):
             data = y_pred[idn]
-            for time in range(0, 29): #  number of seconds
+            for time in range(1, 30): #  number of seconds
                 for x in range(2):
                     if x == 0:
                         z = 'x'
                     else: 
                         z = 'y'
-                    idd = "{}_{}_{}".format(idn, z, time+1)
+                    idd = "{}_{}_{}".format(idn, z, time)
                     idcolumn.append(idd)
-                    locolumn.append(data[time*2+x])
-                    
+                    locolumn.append(data[time*2+x]) # <- this is funky         
+        
 
         data = {'id': idcolumn,
         'location': locolumn
         }
+        
         df = pd.DataFrame(data, columns= ['id', 'location'])
         df.to_csv('submission.csv', index=False, header=True)
 
-        print(df)
+        # print(df)
+       '''
 
     
                     
